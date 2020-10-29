cb886af2660ecf5cdb5955712f9ea0efc64e7333
Federico Soncini <federicosoncini@Federicos-Air.fritz.box>
2020-10-29 12:39:44 +0100

    Created Album "constructor"

---
+++ discography.js
@@ -0,0 +1,162 @@
+const Discography = {
+  albums: [
+    {
+      name: "Queen 2",
+      year: "1974",
+      number_of_songs: "16",
+      picture:
+        "https://images-na.ssl-images-amazon.com/images/I/71B34LLm%2B9L._SY355_.jpg",
+      songs: [
+        {
+          code: "6Wh3qXoyxfK3KeZG1Hwyik",
+          title: "Procession - Remastered 2011",
+          duration: "1:13",
+        },
+        {
+          code: "4ODgFAlSa4RSeugPEXJGcQ",
+          title: "Father To Son - Remastered 2011",
+          duration: "6:13",
+        },
+        {
+          code: "3wGenfAuuEqPDvMK8NNItU",
+          title: "White Queen(As It Began) - Remastered 2011",
+          duration: "4:33",
+        },
+        {
+          code: "29uME0X8n4fNwNqZ6bKZqg",
+          title: "Some Day One Day - Remastered 2011",
+          duration: "4:22",
+        },
+        {
+          code: "3wGenfAuuEqPDvMK8NNItU",
+          title: "White Queen(As It Began) - Remastered 2011",
+          duration: "4:33",
+        },
+        {
+          code: "3wGenfAuuEqPDvMK8NNItU",
+          title: "White Queen(As It Began) - Remastered 2011",
+          duration: "4:33",
+        },
+        {
+          code: "3wGenfAuuEqPDvMK8NNItU",
+          title: "White Queen(As It Began) - Remastered 2011",
+          duration: "4:33",
+        },
+        {
+          code: "3wGenfAuuEqPDvMK8NNItU",
+          title: "White Queen(As It Began) - Remastered 2011",
+          duration: "4:33",
+        },
+        {
+          code: "3wGenfAuuEqPDvMK8NNItU",
+          title: "White Queen(As It Began) - Remastered 2011",
+          duration: "4:33",
+        },
+        {
+          code: "3wGenfAuuEqPDvMK8NNItU",
+          title: "White Queen(As It Began) - Remastered 2011",
+          duration: "4:33",
+        },
+        {
+          code: "3wGenfAuuEqPDvMK8NNItU",
+          title: "White Queen(As It Began) - Remastered 2011",
+          duration: "4:33",
+        },
+        {
+          code: "3wGenfAuuEqPDvMK8NNItU",
+          title: "White Queen(As It Began) - Remastered 2011",
+          duration: "4:33",
+        },
+        {
+          code: "3wGenfAuuEqPDvMK8NNItU",
+          title: "White Queen(As It Began) - Remastered 2011",
+          duration: "4:33",
+        },
+        {
+          code: "3wGenfAuuEqPDvMK8NNItU",
+          title: "White Queen(As It Began) - Remastered 2011",
+          duration: "4:33",
+        },
+      ],
+    },
+    {
+      name: "Sheer Heart Attack",
+      year: "1974",
+      number_of_songs: "18",
+      picture:
+        "https://images-na.ssl-images-amazon.com/images/I/91k9o225K6L._SX450_.jpg",
+      songs: [
+        {
+          code: "6Wh3qXoyxfK3KeZG1Hwyik",
+          title: "Brighton Rock",
+          duration: "1:13",
+        },
+        {
+          code: "4ODgFAlSa4RSeugPEXJGcQ",
+          title: "Killer Queen",
+          duration: "6:13",
+        },
+        {
+          code: "3wGenfAuuEqPDvMK8NNItU",
+          title: "Tenement Funster",
+          duration: "4:33",
+        },
+        {
+          code: "29uME0X8n4fNwNqZ6bKZqg",
+          title: "Some Day One Day - Remastered 2011",
+          duration: "4:22",
+        },
+        {
+          code: "3wGenfAuuEqPDvMK8NNItU",
+          title: "White Queen(As It Began) - Remastered 2011",
+          duration: "4:33",
+        },
+        {
+          code: "3wGenfAuuEqPDvMK8NNItU",
+          title: "White Queen(As It Began) - Remastered 2011",
+          duration: "4:33",
+        },
+        {
+          code: "3wGenfAuuEqPDvMK8NNItU",
+          title: "White Queen(As It Began) - Remastered 2011",
+          duration: "4:33",
+        },
+        {
+          code: "3wGenfAuuEqPDvMK8NNItU",
+          title: "White Queen(As It Began) - Remastered 2011",
+          duration: "4:33",
+        },
+        {
+          code: "3wGenfAuuEqPDvMK8NNItU",
+          title: "White Queen(As It Began) - Remastered 2011",
+          duration: "4:33",
+        },
+        {
+          code: "3wGenfAuuEqPDvMK8NNItU",
+          title: "White Queen(As It Began) - Remastered 2011",
+          duration: "4:33",
+        },
+        {
+          code: "3wGenfAuuEqPDvMK8NNItU",
+          title: "White Queen(As It Began) - Remastered 2011",
+          duration: "4:33",
+        },
+        {
+          code: "3wGenfAuuEqPDvMK8NNItU",
+          title: "White Queen(As It Began) - Remastered 2011",
+          duration: "4:33",
+        },
+        {
+          code: "3wGenfAuuEqPDvMK8NNItU",
+          title: "White Queen(As It Began) - Remastered 2011",
+          duration: "4:33",
+        },
+        {
+          code: "3wGenfAuuEqPDvMK8NNItU",
+          title: "White Queen(As It Began) - Remastered 2011",
+          duration: "4:33",
+        },
+      ],
+    },
+  ],
+};
--- script.js
+++ script.js
@@ -1,23 +1,55 @@
-const Album = {
+/* const Album = {
   name: "Queen 2",
   year: 1974,
   picture:
     "https://images-na.ssl-images-amazon.com/images/I/71B34LLm%2B9L._SY355_.jpg",
-    assignPicture: function() {
-        //get Dom element and assign
-        let src = document.querySelector(".card-img-top");
-        
-    }
+
+  loadPicture: function () {
+    //load Picture in Artist Page
+    let img = document.querySelector(".card.h-100 img");
+    img.setAttribute("src", this.picture);
+  },
 
   playSong: function () {
-    //change icon of song
-
-    let icon = document.querySelector(".table th i");
-    icon.style.color = "red";
-    console.log(icon);
-
-    //make white overlay
-
-    //playsong
+    //TODO playsong
   },
+
+ 
+}; */
+
+const Album = {
+  name: "",
+  year: "",
+  picture: "",
+  loadPicture: function () {
+    //load Picture in Artist Page
+    let img = document.querySelector(".card.h-100 img");
+    img.setAttribute("src", this.picture);
+  },
+  playSong: function () {
+    //TODO playsong
+  },
+  songList: [],
+};
+
+window.onload = function () {
+  //Album.loadPicture(); //TODO make it into a constructor later
+
+  //let currentAlbum = new Album(code);
+  let current_album = Discography.albums[1];
+  let Album_instance = Object.create(Album);
+
+  Album_instance.name = current_album.name;
+  Album_instance.year = current_album.year;
+  Album_instance.picture = current_album.picture;
+  Album_instance.songList = current_album.songs;
+
+  Album_instance.loadPicture();
+
+  let icons = document.querySelectorAll(".table th i");
+  for (let i = 0; i < icons.length; i++) {
+    icons[i].addEventListener("click", function () {
+      icons[i].classList.add("selected");
+    });
+  }
 };
